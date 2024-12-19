cd7e91bad1a77748df70d6429889fa6932493fec
Inma Gijón <igijoninf@gmail.com>
2024-12-19 17:03:02 +0100

    Nuevo componente con fragmentos

---
+++ src/FirstApp.jsx
@@ -0,0 +1,9 @@
+
+export const FirstApp = () => {
+    return ( //Fragmentos
+        <> 
+            <h1>First App Component</h1>
+            <p>Soy un subtítulo</p>
+        </>
+    );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import {createRoot} from 'react-dom/client';
 
 import { HelloWorldApp } from './HelloWorldApp';
+import { FirstApp } from './FirstApp';
 
 const root = createRoot(document.getElementById('root'));
-root.render(<HelloWorldApp/>);
+//root.render(<HelloWorldApp/>);
+root.render(<FirstApp/>);
