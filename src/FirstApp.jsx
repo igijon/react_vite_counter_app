eda44915b66e840b4e5ef9526339c504c2a38196
Inma Gijón <igijoninf@gmail.com>
2024-12-19 17:09:29 +0100

    Impresión de variable array como elementos separados aparentemente juntos

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,8 +1,8 @@
-
+const newMessage = [1,2,3,4,5,6,7];  //Forma parte del componente, no está definido en el ámbito global
 export const FirstApp = () => {
     return ( //Fragmentos
         <> 
-            <h1>First App Component</h1>
+            <h1>{ newMessage }</h1>
             <p>Soy un subtítulo</p>
         </>
     );
